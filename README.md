d329b9d8e51a3ad54a7b443f4f5894028a000c34
noemiamccarthy <noemia.mccarthy@code.berlin>
2024-03-12 11:24:59 +0100

    hand_in_02

---
+++ README.md
@@ -0,0 +1,11 @@
+Project Idea - SF B'n'b
+
+Selma and Felix moved to Munich. All their friends and family want to visit them. As there is a long list of people wanting to visit, my project aims to help them plan their guests better.
+The SF B'n'b enables Selma and Felix's friends to find dates when they want to visit. As well as find out more about what activities they can do e.g. skiing or hiking.
+
+Key Features:
+1. Landing page displaying all the SF attractions 
+2. Authentication / login for guests to create profiles 
+3. Guests can book trips and review past trips with profile 
+4. Guests reviews are stored in database 
+5. All people can read reviews on the website
---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+@app.route('/account.html')
+def account():
+    return render_template('account.html')
+
+@app.route('/profile', methods = ["GET"])
+def profile():
+    if request.method == "GET":
+        # getting input with name = fname in HTML form
+        first_name = request.args.get("fname")
+        # getting input with name = sname in HTML form 
+        sur_name = request.args.get("sname")
+
+        print(first_name)
+        print(sur_name)
+ 
+        return render_template('profile.html', fname=first_name, sname=sur_name)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
