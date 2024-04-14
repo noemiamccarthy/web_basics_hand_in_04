23b57f2a05664b7b72a7338caf152048619f6ed7
noemiamccarthy <noemia.mccarthy@code.berlin>
2024-04-14 17:20:57 +0200

    added redirect for eah new review

--- app.py
+++ app.py
@@ -128,8 +128,7 @@ def reviews():
                 cursor.execute("INSERT INTO reviews (Name, Description, Rating, Votes) VALUES (?, ?, ?,?)", (name, description, rating, votes))
                 con.commit()
                 print("Rating created successfully!")
-                reviews = getreviews()
-                return render_template('trips.html') #
+                return redirect('/trips') 
             else:
                 # Handle connection error (e.g., display error message)
                 pass
@@ -197,56 +196,7 @@ def downvotereview(reviewid):
             print(f"Error logging user: {error}")
             # Handle database error (e.g., display error message)
             pass        
-# login_manager = LoginManager(app)
-# login_manager.login_view = "login"
-# class User(UserMixin):
-#         def __init__(self, id, email, password):
-#             self.id = encode(id)
-#             self.email = email
-#             self.password = password
-#             self.authenticated = False
-#         def is_active(self):
-#             return self.is_active()
-#         def is_anonymous(self):
-#             return False
-#         def is_authenticated(self):
-#             return self.authenticated
-#         def is_active(self):
-#             return True
-#         def get_id(self):
-#             return self.id
-        
-# @login_manager.user_loader
-# def load_user(user_id):
-#             conn = sqlite3.connect('/var/www/flask/login.db')
-#             curs = conn.cursor()
-#             curs.execute("SELECT * from login where id = (?)",[user_id])
-#             lu = curs.fetchone()
-#             if lu is None:
-#                 return None
-#             else:
-#                 return User(int(lu[0]), lu[1], lu[2])
-            
-# @app.route("/login", methods=['GET','POST'])
-# def login():
-#   if current_user.is_authenticated:
-#      return redirect(url_for('profile'))
-#   form = LoginForm()
-#   if form.validate_on_submit():
-#      conn = sqlite3.connect('/var/www/flask/accounts.db')
-#      curs = conn.cursor()
-#      curs.execute("SELECT * FROM login where email = (?)",    [form.email.data])
-#      user = list(curs.fetchone())
-#      Us = load_user(user[0])
-#      if form.email.data == Us.email and form.password.data == Us.password:
-#         login_user(Us, remember=form.remember.data)
-#         Umail = list({form.email.data})[0].split('@')[0]
-#         flash('Logged in successfully '+Umail)
-#         redirect(url_for('profile'))
-#      else:
-#         flash('Login Unsuccessfull.')
-#   return render_template('login.html',title='Login', form=form)
-    
+
 if __name__ == "__main__":
     #app.run(debug=True)
     username = request.form.get("username")
